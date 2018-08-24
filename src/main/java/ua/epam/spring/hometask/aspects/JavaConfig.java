7f9138aa3de559f760c0507325dfec754287cfdf
Yauheni Kuks <Yauheni_Kuks@epam.com>
2018-08-24 13:08:17 +0300

    spring java config added

---
+++ src/main/java/ua/epam/spring/hometask/aspects/JavaConfig.java
@@ -0,0 +1,20 @@
+package ua.epam.spring.hometask.aspects;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration 
+@ComponentScan("spring.hometask.*")
+@PropertySource("auditoriums.properties")
+@EnableAspectJAutoProxy
+public class JavaConfig {
+ 
+ @Bean
+ public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+     return new PropertySourcesPlaceholderConfigurer();
+ }
+}
